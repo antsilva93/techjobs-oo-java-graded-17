9e8605be0fc53fea7642218ca9a0008330783ba4
antsilva93 <antsilva93@gmail.com>
2023-10-19 20:11:04 -0500

    task 4 passing, unfinished with full constructor test

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,34 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId() {
+        Job jobOne = new Job();
+        Job jobTwo = new Job();
+        assertNotEquals(jobOne, jobTwo);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        //5 assert true assigning class needed still
+        assertEquals(job.getName(), "Product tester");
+        assertEquals(job.getEmployer().getValue(),"ACME");
+        assertEquals(job.getLocation().getValue(), "Desert");
+        assertEquals(job.getPositionType().getValue(),"Quality control");
+        assertEquals(job.getCoreCompetency().getValue(), "Persistence");
+    }
+    @Test
+    public void testJobsForEquality() {
+        Job jobOne = new Job("Fry Cook", new Employer("Krusty Krab"), new Location("Bikini Bottom"), new PositionType("Cook"), new CoreCompetency("Jellyfishing"));
+        Job jobTwo = new Job("Fry Cook", new Employer("Krusty Krab"), new Location("Bikini Bottom"), new PositionType("Cook"), new CoreCompetency("Jellyfishing"));
+        assertFalse(jobOne.equals(jobTwo));
+        //assertNotEquals(jobOne, jobTwo);
+    }
 }
